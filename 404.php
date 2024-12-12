d88dde3cd15f1332d696ae5312be41bce6bf453d
Matias Di Iorio <matiasmd8610@gmail.com>
2024-12-12 15:23:58 -0300

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="theme-color" content="#2F4598">
+  <meta name="robots" content="index, follow, all">
+  <meta name="googlebot" content="index, follow, all">
+  <meta name="keywords" content="piscina, piscinas, piletero, pileteros, desagote">
+  <meta name="title" content="INAGUA | Pileteros - Limpieza de piscinas - Desagote de piscinass">
+  <meta name="author" content="Administrator">
+  <meta name="description" content="Empresa de Mantenimiento Integral de Piscinas y afines en Capital Federal y alrededores.">
+  <title>INAGUA | Pileteros - Limpieza de piscinas | Error 404</title>
+  <meta name="google-site-verification" content="VtdXUgSAs18OSNEOuGEqfMCEsFGjHU_Z447jcwFi3Fo">
+  <link rel="canonical" href="https://www.inagua.com.ar/">
+  <link rel="icon" href="assets/images/logo.png">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="assets/styles/css/main.css" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;600&family=Rubik:wght@300;400;600&display=swap"
+    rel="stylesheet">
+
+  <!-- Google Tag Manager -->
+  <script>
+    (function (w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-T4FLXK7');
+  </script>
+  <!-- End Google Tag Manager -->
+
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-5539254-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-5539254-2');
+  </script>
+
+  <!-- Facebook Pixel Code -->
+  <script>
+    ! function (f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function () {
+        n.callMethod ?
+          n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = '2.0';
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s)
+    }(window, document, 'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '555542968572021');
+    fbq('track', 'PageView');
+  </script>
+  <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=555542968572021&ev=PageView&noscript=1" /></noscript>
+  <!-- End Facebook Pixel Code -->
+
+</head>
+
+<body>
+  <span class="ir-arriba"><img src="assets/images/up.png" alt="Flecha arriba"></span>
+  <div id="top"></div>
+
+  <!-- HEADER - Begin -->
+  <header>
+    <div class="botoner_completa animated slideInDown">
+      <?php include "include/navegacion.php"; ?>
+    </div>
+  </header>
+  <!-- HEADER - End -->
+
+  <section class="page-404">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="div-texto">
+            <span class="text-404 texto2">404</span>
+            <h1 class="texto1 wow zoomInDown" datos-wow-duracion="2s">OPS! Esta página no existe</h1>
+          </div>
+          <div class="col-md-12 presupuesto-ser wow slideInUp">
+            <a href="/" class="boton-presupuesto">Ir la inicio</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- FOOTER: Begin -->
+  <footer>
+    <?php include "include/footer.php"; ?>
+  </footer>
+  <!-- FOOTER: End -->
+
+  <!-- CHAT BOOT: Begin -->
+  <?php include "include/chat.php"; ?>
+  <!-- CHAT BOOT: End -->
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="assets/scripts/bootstrap.min.js"></script>
+  <script src="assets/scripts/fileinput.js" type="text/javascript"></script>
+  <script src="assets/scripts/wow.min.js"></script>
+  <script defer src="assets/scripts/scripts.js"></script>
+  <script>
+    new WOW().init();
+  </script>
+
+</body>
+
+</html>
